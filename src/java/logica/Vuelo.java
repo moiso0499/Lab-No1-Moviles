3d26818ef8269c39992907dde3aadf4bde8186b6
moiso0499 <moiso0499@outlook.com>
2021-04-03 18:45:37 -0600

    Agregadas clases básicas

--- src/java/logica/Vuelo.java
+++ src/java/logica/Vuelo.java
@@ -5,10 +5,88 @@
  */
 package logica;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 /**
  *
  * @author Moi
  */
 public class Vuelo {
+    int id;
+    LocalDate fecha;
+    LocalTime hora_salida;
+    double precio;
+    Avion avion_id;
+    List<Descuento> descuentos;
+
+    public Vuelo() {
+    }
+
+    public Vuelo(int id, LocalDate fecha, LocalTime hora_salida, double precio, Avion avion_id) {
+        this.id = id;
+        this.fecha = fecha;
+        this.hora_salida = hora_salida;
+        this.precio = precio;
+        this.avion_id = avion_id;
+    }
+
+    public Vuelo(int id, LocalDate fecha, LocalTime hora_salida, double precio, Avion avion_id, List<Descuento> descuentos) {
+        this.id = id;
+        this.fecha = fecha;
+        this.hora_salida = hora_salida;
+        this.precio = precio;
+        this.avion_id = avion_id;
+        this.descuentos = descuentos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora_salida() {
+        return hora_salida;
+    }
+
+    public void setHora_salida(LocalTime hora_salida) {
+        this.hora_salida = hora_salida;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Avion getAvion_id() {
+        return avion_id;
+    }
+
+    public void setAvion_id(Avion avion_id) {
+        this.avion_id = avion_id;
+    }
+
+    public List<Descuento> getDescuentos() {
+        return descuentos;
+    }
+
+    public void setDescuentos(List<Descuento> descuentos) {
+        this.descuentos = descuentos;
+    }
     
 }
