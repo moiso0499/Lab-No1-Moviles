caaf369d9815e07b288838c624bffd0a5a4c9d8d
moiso0499 <moiso0499@outlook.com>
2021-04-03 18:44:57 -0600

    Agregadas clases básicas

--- src/java/logica/Descuento.java
+++ src/java/logica/Descuento.java
@@ -10,5 +10,43 @@ package logica;
  * @author Moi
  */
 public class Descuento {
+ 
+    int id;
+    Vuelo vuelo_id;
+    double porcentaje;
+
+    public Descuento() {
+    }
+
+    public Descuento(int id, Vuelo vuelo_id, double porcentaje) {
+        this.id = id;
+        this.vuelo_id = vuelo_id;
+        this.porcentaje = porcentaje;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Vuelo getVuelo_id() {
+        return vuelo_id;
+    }
+
+    public void setVuelo_id(Vuelo vuelo_id) {
+        this.vuelo_id = vuelo_id;
+    }
+
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+    
     
 }
