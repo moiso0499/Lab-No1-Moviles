19ef1d7ad8b4ec1388d4c2d8f21ce9a1c71c9477
moiso0499 <moiso0499@outlook.com>
2021-04-03 18:44:44 -0600

    Agregadas clases básicas

--- src/java/logica/Compra.java
+++ src/java/logica/Compra.java
@@ -5,10 +5,66 @@
  */
 package logica;
 
+import java.util.List;
+
 /**
  *
  * @author Moi
  */
 public class Compra {
+    
+    int id;
+    Usuario usuario_id;
+    Vuelo vuelo_id;
+    List<Tiquete> tiquetes;
+
+    public Compra() {
+    }
+
+    public Compra(int id, Usuario usuario_id, Vuelo vuelo_id) {
+        this.id = id;
+        this.usuario_id = usuario_id;
+        this.vuelo_id = vuelo_id;
+    }
+
+    public Compra(int id, Usuario usuario_id, Vuelo vuelo_id, List<Tiquete> tiquetes) {
+        this.id = id;
+        this.usuario_id = usuario_id;
+        this.vuelo_id = vuelo_id;
+        this.tiquetes = tiquetes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(Usuario usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public Vuelo getVuelo_id() {
+        return vuelo_id;
+    }
+
+    public void setVuelo_id(Vuelo vuelo_id) {
+        this.vuelo_id = vuelo_id;
+    }
+
+    public List<Tiquete> getTiquetes() {
+        return tiquetes;
+    }
+
+    public void setTiquetes(List<Tiquete> tiquetes) {
+        this.tiquetes = tiquetes;
+    }
+    
     
 }
