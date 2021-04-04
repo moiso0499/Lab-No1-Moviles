ced3241125c67efab069c1d11868d73c98005fb8
moiso0499 <moiso0499@outlook.com>
2021-04-03 18:41:04 -0600

    Agregadas clases básicas

--- src/java/logica/Avion.java
+++ src/java/logica/Avion.java
@@ -5,10 +5,67 @@
  */
 package logica;
 
+import java.util.List;
+
 /**
  *
  * @author Moi
  */
 public class Avion {
+    int id;
+    Tipoavion tipoAvion_id;
+    Ruta ruta_id;
+    List<Vuelo> vuelos;
+
+    public Avion() {
+    }
+
+    public Avion(int id, Tipoavion tipoAvion_id, Ruta ruta_id) {
+        this.id = id;
+        this.tipoAvion_id = tipoAvion_id;
+        this.ruta_id = ruta_id;
+    }
+
+    public Avion(int id, Tipoavion tipoAvion_id, Ruta ruta_id, List<Vuelo> vuelos) {
+        this.id = id;
+        this.tipoAvion_id = tipoAvion_id;
+        this.ruta_id = ruta_id;
+        this.vuelos = vuelos;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Tipoavion getTipoAvion_id() {
+        return tipoAvion_id;
+    }
+
+    public void setTipoAvion_id(Tipoavion tipoAvion_id) {
+        this.tipoAvion_id = tipoAvion_id;
+    }
+
+    public Ruta getRuta_id() {
+        return ruta_id;
+    }
+
+    public void setRuta_id(Ruta ruta_id) {
+        this.ruta_id = ruta_id;
+    }
+
+    public List<Vuelo> getVuelos() {
+        return vuelos;
+    }
+
+    public void setVuelos(List<Vuelo> vuelos) {
+        this.vuelos = vuelos;
+    }
+    
     
 }
