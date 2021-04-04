9f11b0b2e8cc86c28b244dc37f77726781d62a8b
moiso0499 <moiso0499@outlook.com>
2021-04-03 18:45:22 -0600

    Agregadas clases básicas

--- src/java/logica/Ubicacion.java
+++ src/java/logica/Ubicacion.java
@@ -10,5 +10,32 @@ package logica;
  * @author Moi
  */
 public class Ubicacion {
+    String codigo;
+    String descripcion;
+
+    public Ubicacion() {
+    }
+
+    public Ubicacion(String codigo, String descripcion) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
     
 }
